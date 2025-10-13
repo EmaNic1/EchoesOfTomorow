6b9a06d4da44dc44908a22fc428327bcfa429220
Ema Nickute <114803991+EmaNic1@users.noreply.github.com>
2025-10-13 22:04:48 +0300

    Character animation and movement

---
+++ Assets/Scripts/CameraMovemnet.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public class CameraMovemnet : MonoBehaviour
+{
+    [SerializeField] Transform target;           // veikėjas (Player)
+    [SerializeField] float smoothSpeed = 5f;     // kaip greitai kamera prisitaiko
+    [SerializeField] Vector3 offset = new Vector3(0, 0, -10); // kad kamera būtų už veikėjo
+
+    void FixedUpdate()
+    {
+        if (target == null)
+            return;
+
+        // Nauja pozicija tik X ir Y ašyse (Z lieka -10)
+        Vector3 desiredPosition = new Vector3(
+            target.position.x + offset.x,
+            target.position.y + offset.y,
+            offset.z
+        );
+
+        // Švelnus judėjimas
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float speed = 2f;
+    Vector2 motionVector;
+    Animator animator;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        motionVector = new Vector2(
+            Input.GetAxisRaw("Horizontal"), 
+            Input.GetAxisRaw("Vertical"));
+
+        animator.SetFloat("horizontal", Input.GetAxisRaw("Horizontal"));
+        animator.SetFloat("vertical", Input.GetAxisRaw("Vertical"));
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.linearVelocity = motionVector * speed;
+    }
+}
