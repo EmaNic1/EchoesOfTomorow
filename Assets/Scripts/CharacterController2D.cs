b6a41ff93d840a57ad1648e48e3392c5d894233d
Ema Nickute <114803991+EmaNic1@users.noreply.github.com>
2025-10-15 14:55:44 +0300

    Cutting trees ant collectong logs

---
+++ Assets/Scripts/CharacterController2D.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class CharacterController2D : MonoBehaviour
+{
+    Rigidbody2D rb;
+    [SerializeField] float speed = 2f;
+    public Vector2 motionVector;
+    public Vector2 lastMotionVector;
+    Animator animator;
+    public bool moving;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        motionVector = new Vector2(horizontal, vertical);
+
+        animator.SetFloat("horizontal", horizontal);
+        animator.SetFloat("vertical", vertical);
+
+        moving = horizontal != 0 || vertical != 0;
+        animator.SetBool("moving", moving);
+
+        if (horizontal != 0 || vertical != 0)
+        {
+            lastMotionVector = new Vector2(horizontal, vertical).normalized;
+            animator.SetFloat("lastHorizontal", horizontal);
+            animator.SetFloat("lastVertical", vertical);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        rb.linearVelocity = motionVector * speed;
+    }
+}
